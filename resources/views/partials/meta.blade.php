25509228c445c7513342aee8f458e411c33d19ad
Meon11 <48332785+Meon11@users.noreply.github.com>
2020-08-28 12:42:27 +0200

    initial commit

---
+++ resources/views/partials/meta.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta content="" name="descriptison">
+<meta content="" name="keywords">
+<META NAME="ROBOTS" CONTENT="NOINDEX, FOLLOW">
+<META NAME="ROBOTS" CONTENT="INDEX, NOFOLLOW">
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
